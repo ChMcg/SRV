9fac97387d5ed8aeb028d1851d362c8af4c2bea0
Bakhir A.D <Bakhir.A.D.st@gmail.com>
2022-05-02 23:55:52 +0300

    lab_4 final version

--- 4_Bakhir_7361.c
+++ 4_Bakhir_7361.c
@@ -1,3 +1,29 @@
+/*
+ * Выполнил: Андрей Бахир группа 7361
+ * Задание: №3 Разгрузка контейнеров
+ * Дата выполнения: 02.05.2022
+ * Версия: 0.1
+ *
+ * Скрипт для компиляции и запуска программы:
+ *      <компиляция>: gcc -Wno-incompatible-pointer-types -pthread -lrt 4_Bakhir_7361.c -o 4_Bakhir_7361
+ *      <запуск>: ./4_Bakhir_7361
+ */
+// ------------------------------------------- // 
+/*
+ * Общее описание программы:
+ *      Эмулируется работа системы в которой диспетчер регулирует
+ *      процесс разгрузки трёх контейнеров. Контейнер эмулируется 
+ *      потоком, создающимся по таймеру. Диспетчер имеет свой 
+ *      постоянный поток в котором производит регуляцию.
+ * 
+ * Организация очерёдности: 
+ *      Контейнеры после своего создания атомарно добавляют всю 
+ *      необходимую информацию в очередь. Диспетчер также 
+ *      атомарно получает эту информацию из очереди и отдаёт 
+ *      сигналы ожидающим контейнерам.
+ */
+// ------------------------------------------- //
+
 #define _GNU_SOURCE
 
 #include <stdlib.h>
@@ -64,8 +90,6 @@ struct sigevent* initialize_event(int signumber, int container_id);
 void initialize_signal(int signumber, void(*handler)(sigval_t));
 char* now();
 void emit_cond_signal(pthread_cond_t* cond, pthread_mutex_t* mutex);
-
-
 
 
 
@@ -189,6 +213,7 @@ int sec_to_ns(float seconds)
     return (int)(seconds * 1000000);
 }
 
+
 void fill_timerspec(struct itimerspec* spec, long int time_ns, long int interval_time_ns)
 {
     int value_sec = time_ns / 1000000;
@@ -201,7 +226,6 @@ void fill_timerspec(struct itimerspec* spec, long int time_ns, long int interval
     spec->it_interval.tv_sec = i_value_sec;
     spec->it_interval.tv_nsec = i_value_nsec;
 }
-
 
 
 struct sigevent* initialize_event(int signumber, int container_id)
@@ -311,10 +335,12 @@ void put_container_id_in_queue(int container_id, pthread_cond_t* cond_var_allow_
     pthread_mutex_unlock(&m_queue);
 }
 
+
 bool is_queue_empty()
 {
     return (q_head == NULL);
 }
+
 
 void print_queue_state()
 {
@@ -338,6 +364,7 @@ void print_queue_state()
     }
 }
 
+
 struct queue_node* create_queue_node(int container_id, pthread_cond_t* cond_var_allow_unload, pthread_mutex_t* m_cond)
 {
     struct queue_node* node;
@@ -349,3 +376,120 @@ struct queue_node* create_queue_node(int container_id, pthread_cond_t* cond_var_
     return node;
 }
 
+
+
+/**
+ * $ gcc -Wno-incompatible-pointer-types -pthread -lrt 4_Bakhir_7361.c -o build/4_Bakhir_7361 && ./build/4_Bakhir_7361 | ccze -A 
+ * 
+ * Intitializing event for container 0 (signal: 35) 
+ * Intitializing event for container 1 (signal: 36) 
+ * Intitializing event for container 2 (signal: 37) 
+ * 23:43:35.341653	| [Dispatcher]: Cycle started 
+ * 23:43:35.341706	| [Dispatcher]: Waiting for new containers added 
+ * 23:43:40.341955	| [Container 0]: created in thread 0x12d06700 
+ * 23:43:40.342100	| [Container 0]: Waiting fow allowance 
+ * 23:43:40.342146	| [Container 1]: created in thread 0x12505700 
+ * 23:43:40.342244	| [Container 1]: Waiting fow allowance 
+ * 23:43:40.342374	| [Container 2]: created in thread 0x11d04700 
+ * 23:43:40.342448	| [Container 2]: Waiting fow allowance 
+ * 23:43:40.342851	| [Dispatcher]: Queue state: (0)-(1)-(2) 
+ * 23:43:40.342919	| [Dispatcher]: Queue not empty, working on 
+ * 23:43:40.342935	| [Dispatcher]: Dispatcher maked his descision: 0 loads out 
+ * 23:43:40.342958	| [Dispatcher]: Waiting container 0 to end his load out 
+ * 23:43:40.343019	| [Container 0]: Unloading 
+ * 23:43:42.343605	| [Container 0]: Unloaded, end of cycle 
+ * 23:43:42.343788	| [Dispatcher]: Cycle started 
+ * 23:43:42.343840	| [Dispatcher]: Queue state: (1)-(2) 
+ * 23:43:42.343866	| [Dispatcher]: Queue not empty, working on 
+ * 23:43:42.343878	| [Dispatcher]: Dispatcher maked his descision: 1 loads out 
+ * 23:43:42.343908	| [Dispatcher]: Waiting container 1 to end his load out 
+ * 23:43:42.343971	| [Container 1]: Unloading 
+ * 23:43:44.344504	| [Container 1]: Unloaded, end of cycle 
+ * 23:43:44.344679	| [Dispatcher]: Cycle started 
+ * 23:43:44.344732	| [Dispatcher]: Queue state: (2) 
+ * 23:43:44.344755	| [Dispatcher]: Queue not empty, working on 
+ * 23:43:44.344767	| [Dispatcher]: Dispatcher maked his descision: 2 loads out 
+ * 23:43:44.344798	| [Dispatcher]: Waiting container 2 to end his load out 
+ * 23:43:44.344867	| [Container 2]: Unloading 
+ * 23:43:45.341889	| [Container 0]: created in thread 0x12505700 
+ * 23:43:45.342039	| [Container 0]: Waiting fow allowance 
+ * 23:43:45.342183	| [Container 1]: created in thread 0x12d06700 
+ * 23:43:45.342248	| [Container 1]: Waiting fow allowance 
+ * 23:43:45.342316	| [Container 2]: created in thread 0x11503700 
+ * 23:43:45.342386	| [Container 2]: Waiting fow allowance 
+ * 23:43:46.345361	| [Container 2]: Unloaded, end of cycle 
+ * 23:43:46.345534	| [Dispatcher]: Cycle started 
+ * 23:43:46.345587	| [Dispatcher]: Queue state: (0)-(1)-(2) 
+ * 23:43:46.345617	| [Dispatcher]: Queue not empty, working on 
+ * 23:43:46.345629	| [Dispatcher]: Dispatcher maked his descision: 0 loads out 
+ * 23:43:46.345659	| [Dispatcher]: Waiting container 0 to end his load out 
+ * 23:43:46.345725	| [Container 0]: Unloading 
+ * 23:43:48.346081	| [Container 0]: Unloaded, end of cycle 
+ * 23:43:48.346247	| [Dispatcher]: Cycle started 
+ * 23:43:48.346300	| [Dispatcher]: Queue state: (1)-(2) 
+ * 23:43:48.346325	| [Dispatcher]: Queue not empty, working on 
+ * 23:43:48.346338	| [Dispatcher]: Dispatcher maked his descision: 1 loads out 
+ * 23:43:48.346367	| [Dispatcher]: Waiting container 1 to end his load out 
+ * 23:43:48.346428	| [Container 1]: Unloading 
+ * 23:43:50.341914	| [Container 0]: created in thread 0x12505700 
+ * 23:43:50.342024	| [Container 0]: Waiting fow allowance 
+ * 23:43:50.342097	| [Container 1]: created in thread 0x11d04700 
+ * 23:43:50.342159	| [Container 1]: Waiting fow allowance 
+ * 23:43:50.342183	| [Container 2]: created in thread 0x10d02700 
+ * 23:43:50.342235	| [Container 2]: Waiting fow allowance 
+ * 23:43:50.346567	| [Container 1]: Unloaded, end of cycle 
+ * 23:43:50.346697	| [Dispatcher]: Cycle started 
+ * 23:43:50.346748	| [Dispatcher]: Queue state: (2)-(0)-(1)-(2) 
+ * 23:43:50.346780	| [Dispatcher]: Queue not empty, working on 
+ * 23:43:50.346792	| [Dispatcher]: Dispatcher maked his descision: 2 loads out 
+ * 23:43:50.346819	| [Dispatcher]: Waiting container 2 to end his load out 
+ * 23:43:50.346885	| [Container 2]: Unloading 
+ * 23:43:52.347268	| [Container 2]: Unloaded, end of cycle 
+ * 23:43:52.347436	| [Dispatcher]: Cycle started 
+ * 23:43:52.347489	| [Dispatcher]: Queue state: (0)-(1)-(2) 
+ * 23:43:52.347518	| [Dispatcher]: Queue not empty, working on 
+ * 23:43:52.347530	| [Dispatcher]: Dispatcher maked his descision: 0 loads out 
+ * 23:43:52.347562	| [Dispatcher]: Waiting container 0 to end his load out 
+ * 23:43:52.347634	| [Container 0]: Unloading 
+ * 23:43:54.348124	| [Container 0]: Unloaded, end of cycle 
+ * 23:43:54.348289	| [Dispatcher]: Cycle started 
+ * 23:43:54.348341	| [Dispatcher]: Queue state: (1)-(2) 
+ * 23:43:54.348367	| [Dispatcher]: Queue not empty, working on 
+ * 23:43:54.348379	| [Dispatcher]: Dispatcher maked his descision: 1 loads out 
+ * 23:43:54.348407	| [Dispatcher]: Waiting container 1 to end his load out 
+ * 23:43:54.348467	| [Container 1]: Unloading 
+ * 23:43:55.341787	| [Container 0]: created in thread 0x12505700 
+ * 23:43:55.341889	| [Container 0]: Waiting fow allowance 
+ * 23:43:55.341963	| [Container 1]: created in thread 0x11503700 
+ * 23:43:55.342027	| [Container 1]: Waiting fow allowance 
+ * 23:43:55.342103	| [Container 2]: created in thread 0x12d06700 
+ * 23:43:55.342156	| [Container 2]: Waiting fow allowance 
+ * 23:43:56.348958	| [Container 1]: Unloaded, end of cycle 
+ * 23:43:56.349145	| [Dispatcher]: Cycle started 
+ * 23:43:56.349200	| [Dispatcher]: Queue state: (2)-(0)-(1)-(2) 
+ * 23:43:56.349234	| [Dispatcher]: Queue not empty, working on 
+ * 23:43:56.349246	| [Dispatcher]: Dispatcher maked his descision: 2 loads out 
+ * 23:43:56.349278	| [Dispatcher]: Waiting container 2 to end his load out 
+ * 23:43:56.349302	| [Container 2]: Unloading 
+ * 23:43:58.349464	| [Container 2]: Unloaded, end of cycle 
+ * 23:43:58.349584	| [Dispatcher]: Cycle started 
+ * 23:43:58.349628	| [Dispatcher]: Queue state: (0)-(1)-(2) 
+ * 23:43:58.349669	| [Dispatcher]: Queue not empty, working on 
+ * 23:43:58.349688	| [Dispatcher]: Dispatcher maked his descision: 0 loads out 
+ * 23:43:58.349715	| [Dispatcher]: Waiting container 0 to end his load out 
+ * 23:43:58.349739	| [Container 0]: Unloading 
+ * 23:44:00.342177	| [Container 0]: created in thread 0x10d02700 
+ * 23:44:00.342282	| [Container 0]: Waiting fow allowance 
+ * 23:44:00.342339	| [Container 1]: created in thread 0x11d04700 
+ * 23:44:00.342395	| [Container 1]: Waiting fow allowance 
+ * 23:44:00.342440	| [Container 2]: created in thread 0xf3fff700 
+ * 23:44:00.342509	| [Container 2]: Waiting fow allowance 
+ * 23:44:00.349916	| [Container 0]: Unloaded, end of cycle 
+ * 23:44:00.349935	| [Dispatcher]: Cycle started 
+ * 23:44:00.349946	| [Dispatcher]: Queue state: (1)-(2)-(0)-(1)-(2) 
+ * 23:44:00.349957	| [Dispatcher]: Queue not empty, working on 
+ * 23:44:00.349961	| [Dispatcher]: Dispatcher maked his descision: 1 loads out 
+ * 23:44:00.349970	| [Dispatcher]: Waiting container 1 to end his load out 
+ * 23:44:00.350044	| [Container 1]: Unloading 
+ * ^C
+ */
